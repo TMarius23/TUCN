3aadc5f9d6021a6f7f8a8ade9e23290e41c6dee3
TMarius23 <mariustrif0323@yahoo.com>
2025-09-22 15:47:17 +0300

    Delete Lab8 directory

--- Lab8/2/IerarhieExceptii/src/A.java
+++
@@ -1,7 +0,0 @@
-public class A {
-
-    public String method3() throws ExceptieN3 {
-        throw new ExceptieN3("Eroare nivel 3");
-    }
-
-}
--- Lab8/2/IerarhieExceptii/src/ExceptieN3.java
+++
@@ -1,9 +0,0 @@
-public class ExceptieN3 extends ExceptieN2 {
-
-    public ExceptieN3() {
-        super();
-    }
-    public ExceptieN3(String message) {
-        super(message);
-    }
-}
